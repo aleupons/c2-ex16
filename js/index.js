56e73c388144844a09ebb6f457b59e93a25fe019
aleupons <apahoquei@gmail.com>
2021-06-07 20:03:56 +0200

    Dibuixar errors, escriure lletres encertades i errònies, parar joc quan falles o encertes

--- js/index.js
+++ js/index.js
@@ -16,28 +16,76 @@ const getRandom = (array) => {
 // Elements del DOM
 const inputParaula = document.querySelector("#paraula");
 inputParaula.value = "";
+const hangman = document.querySelector("#hangman");
+const final = document.querySelector(".final");
+const lletres = document.querySelector(".lletres");
+const errors = document.querySelector(".errors");
 
-// Extraure la paraula a l'àmbit global
+// Variables a l'àmbit global
 let paraulaGlobal;
-const getParaula = (paraula) => (paraulaGlobal = paraula);
+const setParaula = (paraula) => (paraulaGlobal = paraula);
+let comptadorIntents = 1;
+const setComptador = (comptador) => (comptadorIntents = comptador);
 
 // Recollir la lletra de l'input cada cop que s'escriu
 inputParaula.addEventListener("keyup", (e) => {
   if (e.target.value !== "") {
     const lletra = e.target.value.charAt(e.target.value.length - 1);
-    compararLletres(paraulaGlobal.charAt(e.target.value.length - 1), lletra)
-      ? console.log("bé")
-      : console.log("mal");
+    if (comptadorIntents < 12) {
+      const indexs = compararLletres(paraulaGlobal, lletra);
+      if (indexs.length !== 0) {
+        const incognites = lletres.textContent.split(" ");
+        for (const index of indexs) {
+          incognites[index] = lletra;
+        }
+        lletres.textContent = incognites.join(" ");
+      } else {
+        dibuixarError(comptadorIntents, lletra);
+      }
+    } else {
+      final.textContent = "Has perdut.";
+      inputParaula.disabled = true;
+      reiniciar();
+    }
+  } else if (
+    lletres.textContent.split(" ").join("").toLowerCase() ===
+    paraulaGlobal.toLowerCase()
+  ) {
+    final.textContent = "Molt bé, has encertat la paraula.";
+    inputParaula.disabled = true;
+    reiniciar();
   }
 });
 
 // Comparar lletra input amb lletra paraula
-const compararLletres = (lletraParaula, lletraInput) => {
-  if (lletraInput.toLowerCase() === lletraParaula.toLowerCase()) {
-    return true;
-  } else {
-    return false;
+const compararLletres = (paraula, lletra) => {
+  const lletresParaula = paraula.split("");
+  const indexs = [];
+  for (const i in lletresParaula) {
+    if (lletresParaula[i].toLowerCase() === lletra.toLowerCase()) {
+      indexs.push(i);
+    }
   }
+  if (indexs !== []) {
+    return indexs;
+  } else {
+    return [];
+  }
+};
+
+// Dibuixar si l'usuari s'equivoca amb una lletra
+const dibuixarError = (intent, lletra) => {
+  hangman.querySelector(`.stage${intent}`).classList.add("on");
+  setComptador(intent + 1);
+  errors.textContent += ` ${lletra}`;
+};
+
+// Fer aparèixer botó per reiniciar
+const reiniciar = () => {
+  const botoTornarhi = document.createElement("button");
+  botoTornarhi.textContent = "Torna-ho a intentar";
+  document.body.append(botoTornarhi);
+  botoTornarhi.addEventListener("click", () => location.reload());
 };
 
 // Main
@@ -45,6 +93,12 @@ const compararLletres = (lletraParaula, lletraInput) => {
   const dades = await cridaApi();
   const paraules = dades.lista;
   const paraula = getRandom(paraules);
-  getParaula(paraula);
-  console.log(paraula);
+  setParaula(paraula);
+  const incognita = "_ ";
+  let incognites = "";
+  for (const i in paraula.split("")) {
+    incognites += incognita;
+  }
+  incognites = incognites.slice(0, -1);
+  lletres.textContent = incognites;
 })();
